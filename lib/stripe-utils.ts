c95b6e249865b7f7622ff8a4d44891c5c86eedc3
Xto-dev <oleggunkojob@gmail.com>
2026-04-03 14:25:47 +0300

    Fix Stripe API version

--- lib/stripe-utils.ts
+++ lib/stripe-utils.ts
@@ -2,7 +2,6 @@ import Stripe from 'stripe'
 
 // Initialize Stripe
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18', // Use the latest API version suitable for your request
   typescript: true,
 })
 
